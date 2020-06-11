9ebdd2f96ab68754f4ae4d7790a316ed0791be43
Mido Akeel <akeelmido@gmail.com>
2020-06-11 11:52:46 -0400

    adding addR function

--- scripts.js
+++ scripts.js
@@ -2,9 +2,34 @@
 
 var rows = 0;
 var cols = 0;
-var colorChoice;
+var color;
 var colorChange = function (){
-    this.style.backgroundColor = colorSelected;
+    this.style.backgroundColor = color;
     };
 
 //functions 
+
+function addR() {
+    let grid = document.getElementById("grid");
+    let row = document.createElement('tr');
+    let col = document.createElement('td');
+    
+    if(rows === 0){
+	col.onclick = color;
+
+	row.appendChild(col);
+	grid.appendChild(row);
+
+	rows++;
+	cols++;
+    }
+    else{
+	for(let i =0; i<cols; i++){
+	    col.onclick = color;
+	    row.appendChild(col);	    
+	}
+    }
+
+    grid.appendChild(row);
+    rows++;
+}
